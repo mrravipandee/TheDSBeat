8598ad64500e2cc0ee3b2b716146a9c7b7e0564e
Ravi Pandey <imravipanday@gmail.com>
2025-07-05 17:08:50 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome to Ds Beat</h1>
+      <p>Keep the Code Flowing</p>
+    </div>
+  )
+}
+
+export default page
